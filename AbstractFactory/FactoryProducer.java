49224680bcd7ffd52da83c582f36c2f350ce48a5
Prity Das Bornita <114295389+PrityDas@users.noreply.github.com>
2023-08-25 23:50:49 +0600

    Update FactoryProducer.java

--- AbstractFactory/FactoryProducer.java
+++ AbstractFactory/FactoryProducer.java
@@ -5,10 +5,7 @@
  */
 package Design_Pattern.AbstractFactory;
 
-/**
- *
- * @author TANIM
- */
+
 public class FactoryProducer {
     public static AbstractFactory getFactory(String factory){
         if(factory.equalsIgnoreCase("NormalVehicle")){
